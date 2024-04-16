f6e73bcdbe314482d7cc079fdb434084651eb720
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-04-16 20:20:54 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+  <body>
+    <label for="language">Languages :</label>
+    <select id="language" onchange="changeLanguage(this.value)">
+        <option value="fr">Français</option>
+        <option value="en">English</option>
+        <option value="jp">日本語</option>
+    </select>
+    <p id="text" data-translate="text">hello !</p>
+  </body>
+
+  <script>
+    const translations = {
+            "fr": {
+                "text": "Bonjour !",
+            },
+            "en": {
+                "text": "Hello !",
+            },
+            "jp": {
+                "text": "こんにちは",
+            }
+        };
+
+        function changeLanguage(language) {
+            const elements = document.querySelectorAll('[data-translate]');
+            elements.forEach(element => {
+                const key = element.getAttribute('data-translate');
+                element.textContent = translations[language][key];
+            });
+        }
+  </script>
+</html>
